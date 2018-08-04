7c2bfd8ac3cdef1bdde7d0a91a5cb831f1ca6390
Hartnell Foster <hartnell.foster@googlemail.com>
2018-08-04 10:41:59 +0200

    removed ec2_key from variables

--- variables.tf
+++ variables.tf
@@ -7,8 +7,3 @@ variable "vpc_cidr_block" {
   description = "The base CIDR block for the VPC"
   default     = "10.0.0.0/16"
 }
-
-variable "ec2_key_pair" {
-  description = "The AWS Key Pair for accessing hosts"
-  default     = "eu-west-2-test.key"
-}
