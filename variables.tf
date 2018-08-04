2dd33115754368032ae80ee43929e5df61cf0a43
Hartnell Foster <hartnell.foster@googlemail.com>
2018-08-04 12:43:14 +0200

    Deleted commented-out region

--- variables.tf
+++ variables.tf
@@ -2,9 +2,3 @@ variable "vpc_cidr_block" {
   description = "The base CIDR block for the VPC"
   default     = "10.0.0.0/16"
 }
-
-#variable "aws_region" {
-#  description = "The AWS region to create things in"
-#  default     = "eu-west-2"
-#}
-
