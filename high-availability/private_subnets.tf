fa1372102445121fa9c77a470d7bb4e0b178ba2e
Hartnell Foster <hartnell.foster@googlemail.com>
2018-08-18 14:07:28 +0200

    Refactored to dynamically allow for valid private subnet CIDR blocks

--- high-availability/private_subnets.tf
+++ high-availability/private_subnets.tf
@@ -1,8 +1,12 @@
 #Private Subnets
+locals {
+  num_azs = "${length(data.aws_availability_zones.available.names)}"
+}
+
 resource "aws_subnet" "private" {
   count             = "${length(data.aws_availability_zones.available.names)}"
   vpc_id            = "${aws_vpc.main.id}"
-  cidr_block        = "10.2.${count.index}.0/24"
+  cidr_block        = "10.0.${count.index + local.num_azs}.0/24"
   availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
 
   tags {
