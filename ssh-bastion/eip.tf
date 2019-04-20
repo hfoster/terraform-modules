09bac3267d6d5e2efa69ae694a2ffdd017f41c32
Hartnell Foster <hartnell.foster@googlemail.com>
2019-04-20 19:48:30 +0100

    Re-refactoring to bring the Packer template for the AMI into this repo

--- ssh-bastion/eip.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_eip" "bastion_ip" {
-  count = "${length(data.aws_availability_zones.available.names)}"
-  vpc   = true
-
-  tags {
-    Name = "${var.env_name}-bastion-ip-${count.index}"
-  }
-}
