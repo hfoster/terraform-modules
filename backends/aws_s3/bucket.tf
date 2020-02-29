6840f5887f454e357b1adf6cd6f3ab05c577a75a
Hartnell Foster <hartnell.foster@googlemail.com>
2020-02-29 10:16:12 +0000

    Refactored backend bucket module into Terraform v0.12

--- backends/aws_s3/bucket.tf
+++ backends/aws_s3/bucket.tf
@@ -1,8 +1,8 @@
 #S3 Bucket definition for use as a Terraform backend
 resource "aws_s3_bucket" "terraform_backend" {
-  bucket = "${var.bucket_name}"
+  bucket = var.bucket_name
   acl    = "private"
-  region = "${var.aws_region}"
+  region = var.aws_region
 
   versioning {
     enabled = true
--- terraform-backends/s3/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "aws_region" {
-  default = "eu-west-2"
-}
-
-variable "bucket_name" {
-  description = "The name of the backend bucket we are creating. Set with the -var switch at plan time."
-}
