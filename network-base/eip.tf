7076827a321164ae8c3aa590ce3a5652802b0d52
Hartnell Foster <hartnell.foster@googlemail.com>
2020-02-29 10:11:37 +0000

    Refactored `network-base` into six individual AWS Terraform modules

--- network-base/eip.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_eip" "nat_ip" {
-  count = "${length(data.aws_availability_zones.available.names)}"
-  vpc   = true
-
-  tags {
-    Name = "${var.env_name}-nat-ip-${count.index}"
-  }
-}
