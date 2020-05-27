7da8f48073d286367a66c76a6b4d1e5dd1f9db15
Hartnell Foster <hartnellfoster@Dreilide.local>
2020-05-27 14:54:49 +0100

    Making arithmetic clearer

--- aws/subnet/main.tf
+++ aws/subnet/main.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "subnet" {
   count             = local.num_azs
   vpc_id            = var.vpc_id
-  cidr_block        = cidrsubnet(var.vpc_cidr, var.subnet_newbits, count.index + var.offset)
+  cidr_block        = cidrsubnet(var.vpc_cidr, var.subnet_newbits, (count.index + var.offset))
   availability_zone = data.aws_availability_zones.available.names[count.index]
 }
